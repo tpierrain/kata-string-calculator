51478194d0c2d1c49784399ac3d01cc4f90685d3
tpierrain <thomas@pierrain.net>
2021-02-27 12:05:40 +0100

    RED (Add_EmptyString)

--- StringCalculator/StringCalculator.Tests/StringCalculatorShould.cs
+++ StringCalculator/StringCalculator.Tests/StringCalculatorShould.cs
@@ -1,3 +1,5 @@
+using System;
+using NFluent;
 using NUnit.Framework;
 
 namespace StringCalculator.Tests
@@ -5,6 +7,20 @@ namespace StringCalculator.Tests
     [TestFixture]
     public class StringCalculatorShould
     {
-        
+        [Test]
+        public void Add_EmptyString()
+        {
+            var result = StringCalculator.Add("");
+
+            Check.That(result).IsEqualTo(0);
+        }
+    }
+
+    public static class StringCalculator
+    {
+        public static int Add(string numbers)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
