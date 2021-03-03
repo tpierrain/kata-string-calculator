dd9c628a632cb79aecec40eccbd2506a6c742052
tpierrain <thomas@pierrain.net>
2021-03-03 13:40:21 +0100

    First part of kata

--- StringCalculatorTest/StringCalculatorTests/StringCalculatorShould.cs
+++ StringCalculatorTest/StringCalculatorTests/StringCalculatorShould.cs
@@ -7,7 +7,78 @@ namespace StringCalculatorTests
     [TestFixture]
     public class StringCalculatorShould
     {
-        // Start here...
+        [Test]
+        public void Return_zero_when_empty_string_is_given()
+        {
+            var stringCalculator = new StringCalculator();
+            
+            var sum = stringCalculator.Add("");
 
+            Check.That(sum).IsEqualTo(0);
+        }
+
+        [TestCase("1", 1)]
+        [TestCase("2", 2)]
+        public void Return_a_number_when_string_contains_a_number(string numbers, int expected)
+        {
+            var stringCalculator = new StringCalculator();
+
+            var sum = stringCalculator.Add(numbers);
+
+            Check.That(sum).IsEqualTo(expected);
+        }
+
+        [Test]
+        public void Sum_2_numbers()
+        {
+            var stringCalculator = new StringCalculator();
+
+            var sum = stringCalculator.Add("1,2");
+
+            Check.That(sum).IsEqualTo(3);
+        }
+
+        [Test]
+        public void Sum_3_numbers()
+        {
+            var stringCalculator = new StringCalculator();
+
+            var sum = stringCalculator.Add("1,2,1");
+
+            Check.That(sum).IsEqualTo(4);
+        }
+
+        [Test]
+        public void Return_the_sum_when_expression_also_contains_a_newline_as_delimiter()
+        {
+            var stringCalculator = new StringCalculator();
+
+            var sum = stringCalculator.Add("1\n2,3");
+
+            Check.That(sum).IsEqualTo(6);
+        }
+    }
+
+    public class StringCalculator
+    {
+        public int Add(string expression)
+        {
+            if (expression == string.Empty)
+            {
+                return 0;
+            }
+
+            var defaultDelimiter = ',';
+
+            var numbers = expression.Split(defaultDelimiter, '\n');
+
+            var sum = 0;
+            foreach (var number in numbers)
+            {
+                sum += int.Parse(number);
+            }
+
+            return sum;
+        }
     }
 }
